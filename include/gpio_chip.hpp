5405dba1e80d084e79156f6bf36960c9c5b50d25
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2018-08-19 21:45:08 -0400

    gpio::chip: add functions id, pin_count and pin

--- include/gpio_chip.hpp
+++ include/gpio_chip.hpp
@@ -10,8 +10,12 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 #include <gpio_pin.hpp>
+#include <gpio_types.hpp>
+
 #include <memory>
+#include <stdexcept>
 #include <string>
+#include <utility>
 #include <vector>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -22,21 +26,46 @@ namespace gpio
 struct chip
 {
     ////////////////////
+    auto const& id() const { return id_; }
     auto const& name() const { return name_; }
+
+    ////////////////////
+    auto pin_count() const noexcept { return pins_.size(); }
+
+    gpio::pin& pin(gpio::pos n)
+    {
+        throw_range(n);
+        return *pins_[n];
+    }
+    gpio::pin const& pin(gpio::pos n) const
+    {
+        throw_range(n);
+        return *pins_[n];
+    }
 
 protected:
     ////////////////////
-    std::string name_;
+    std::string id_, name_;
 
     using pin_ptr = std::unique_ptr<gpio::pin>;
     std::vector<pin_ptr> pins_;
+
+    ////////////////////
+    chip(std::string id) : id_(std::move(id)) { }
+
+    void throw_range(gpio::pos n) const
+    {
+        if(n >= pin_count()) throw std::out_of_range(
+            "Invalid pin # " + std::to_string(n)
+        );
+    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-extern "C" gpio::chip* create_chip(const char*);
+extern "C" gpio::chip* create_chip(const char* param);
 extern "C" void delete_chip(gpio::chip*);
 
 ////////////////////////////////////////////////////////////////////////////////
